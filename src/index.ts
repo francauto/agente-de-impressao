6a69b00892a63ea4c43076e93689cd1c5ec488f6
Thales Leal <thalesvinicius.leal@gmail.com>
2025-12-11 18:17:13 -0300

    Add environment configuration and update Docker setup
    - Create .env.example for environment variable management
    - Update .gitignore to exclude .env file
    - Modify docker-compose.yml to use environment variables for NODE_ENV and PORT
    - Remove Dockerfile as it's no longer needed
    - Add dotenv dependency to package.json and package-lock.json
    - Refactor index.ts to use PORT from environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-import express from "express"
+import express from "express";
 import { sendToPrinter } from "./printer.service";
 
 const app = express();
 app.use(express.json());
 
-
-app.get('/teste', async (req , res)=>{
-    res.status(200).json('API FUNCIONANDO')
-})
+app.get("/teste", async (req, res) => {
+  res.status(200).json("API FUNCIONANDO");
+});
 
 app.post("/print", async (req, res) => {
   const { id_requisicao, tipo, printer_ip, file_url } = req.body;
 
   if (!id_requisicao || !printer_ip || !file_url)
-    return res.status(400).json({ success: false, error: "Parâmetros inválidos" });
+    return res
+      .status(400)
+      .json({ success: false, error: "Parâmetros inválidos" });
 
-  console.log(`🖨️ Pedido de impressão recebido: ${tipo} #${id_requisicao} → ${printer_ip}`);
+  console.log(
+    `🖨️ Pedido de impressão recebido: ${tipo} #${id_requisicao} → ${printer_ip}`
+  );
 
   const result = await sendToPrinter(file_url, printer_ip);
   res.json({
@@ -26,5 +29,7 @@ app.post("/print", async (req, res) => {
   });
 });
 
-const PORT = 4000;
-app.listen(PORT, () => console.log(`🚀 Agente de Impressão rodando na porta ${PORT}`));
+const PORT = process.env.PORT || 4005;
+app.listen(PORT, () =>
+  console.log(`🚀 Agente de Impressão rodando na porta ${PORT}`)
+);
